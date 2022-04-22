9a99434e9cf56b5eb61d9d01687365a4848e2951
yukke <yukke@yukke-MacBook-Pro.local>
2022-04-22 21:55:20 +0900

    仮spec設定

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -2,5 +2,7 @@
 
 class Api::V1::JobsController < ApplicationController
   def index
+    jobs = Job.all
+    render status: 200, json: { items: jobs }
   end
 end
--- spec/requests/api/v1/jobs_spec.rb
+++ spec/requests/api/v1/jobs_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe "Api::V1::Jobs", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/api/v1/jobs/index"
+      get api_v1_jobs_path
       expect(response).to have_http_status(:success)
     end
   end
