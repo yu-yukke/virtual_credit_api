e0b4f67efc812a84cd90577928569033f856e877
yukke <yukke@yukke-MacBook-Pro.local>
2022-04-26 09:05:15 +0900

    レスポンスにユーザー数を追加

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class JobSerializer < ApplicationSerializer
-  attributes :id, :name, :ancestry
+  attributes :id, :name, :ancestry, :number_of_users
+
+  def number_of_users
+    object.users.count
+  end
 end
