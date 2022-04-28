7315557f6fc9cb7d62bf6b1c2c9b2c5565e72a6c
yukke <yukke@yukke-MacBook-Pro.local>
2022-04-28 23:25:24 +0900

    必要ファイル作成

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string(191)      not null
+#  ancestry   :string(191)
+#  deleted_at :datetime
+#  created_at :datetime
+#  updated_at :datetime
+#
+# Indexes
+#
+#  index_categories_on_deleted_at  (deleted_at)
+#
+require "rails_helper"
+
+RSpec.describe Job, type: :model do
+end
