e662092258a6efeb19617e19be893a324370f18b
yukke <yukke@yukke-MacBook-Pro.local>
2022-04-28 23:42:21 +0900

    コントローラとrequest spec

---
+++ app/serializers/category_serializer.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string(191)      not null
+#  ancestry   :string(191)
+#  deleted_at :datetime
+#  created_at :datetime
+#  updated_at :datetime
+#
+# Indexes
+#
+#  index_categories_on_deleted_at  (deleted_at)
+#
+class CategorySerializer < ApplicationSerializer
+  attributes :id, :name, :ancestry, :number_of_products
+
+  def number_of_products
+    object.products.count
+  end
+end
