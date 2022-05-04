8618f21fd8601d02ec814dfdf936249b571d4a7e
塚田治幸 <tsukadaharuyuki@tsukadaharuyukinoMacBook-Air.local>
2022-05-05 01:36:40 +0900

    controllerとrequest spec作成

---
+++ app/serializers/release_note_serializer.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: release_notes
+#
+#  id          :bigint           not null, primary key
+#  version     :decimal(10, )    not null
+#  subject     :string(191)      not null
+#  description :text(65535)      not null
+#  released_at :datetime         not null
+#  deleted_at  :datetime
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+# Indexes
+#
+#  index_release_notes_on_deleted_at  (deleted_at)
+#
+class ReleaseNoteSerializer < ApplicationSerializer
+  attributes :id, :version, :subject, :description, :released_at
+end
