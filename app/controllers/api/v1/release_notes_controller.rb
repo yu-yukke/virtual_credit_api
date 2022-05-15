217ef3e7ce9e25d6ea8f2f31e58c68c5f7905825
塚田治幸 <tsukadaharuyuki@tsukadaharuyukinoMacBook-Air.local>
2022-05-15 21:46:17 +0900

    controllerとrequest spec作成

--- app/controllers/api/v1/release_notes_controller.rb
+++ app/controllers/api/v1/release_notes_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::ReleaseNotesController < ApplicationController
-  before_action :find_release_note, only: %i(show)
+  before_action :find_release_note, only: %i(show update destroy)
 
   def index
     release_notes = ReleaseNote.all
@@ -23,12 +23,37 @@ class Api::V1::ReleaseNotesController < ApplicationController
     end
   end
 
+  def update
+    if @release_note.update update_release_note_params
+      render json: {}, status: 204
+    else
+      render json: { errors: @release_note.errors.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    if @release_note.destroy
+      render json: {}, status: 204
+    else
+      render json: { errors: @release_note.errors.full_messages }, status: 422
+    end
+  end
+
   private
     def find_release_note
       @release_note = ReleaseNote.find params[:id]
     end
 
     def release_note_params
+      params.permit(
+        :version,
+        :subject,
+        :description,
+        :released_at
+      )
+    end
+
+    def update_release_note_params
       params.permit(
         :version,
         :subject,
