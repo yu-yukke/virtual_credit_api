aeb61c7868a34b03c2bb4bdbeb0397d1ab746c01
yukke <yukke@yukke-MacBook-Pro.local>
2022-07-24 23:45:29 +0900

    モデルスペック追記

--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -41,5 +41,6 @@ RSpec.describe Work, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:main_image_url) }
   end
 end
