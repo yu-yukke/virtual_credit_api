540914faadbd24ec89d085a10e78798daf2c881e
yukke <yukke@yukke-MacBook-Pro.local>
2022-09-28 08:42:18 +0900

    model spec実装

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  name            :string(191)      not null
+#  description     :string(191)
+#  main_image_url  :string(255)
+#  thumb_image_url :string(255)
+#  deleted_at      :datetime
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+# Indexes
+#
+#  index_users_on_deleted_at  (deleted_at)
+#
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  describe "associations" do
+    it { should have_one(:user_authentication) }
+    it { should have_one(:social) }
+
+    it { should have_many(:favorites) }
+    it { should have_many(:favorite_works) }
+    it { should have_many(:job_mappings) }
+    it { should have_many(:jobs) }
+    it { should have_many(:creator_mappings) }
+    it { should have_many(:works) }
+    it { should have_many(:tags) }
+    it { should have_many(:assets) }
+    it { should have_many(:inquiries) }
+  end
+end
